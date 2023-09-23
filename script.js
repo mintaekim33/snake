d3acc747ed89d7c335e6c9460124496c4e2061a1
mintaekim <kmintae33@gmail.com>
2023-09-23 16:46:47 +0800

    tried making the snake come out from the opp side when reaching the end

--- script.js
+++ script.js
@@ -1,1 +1,108 @@
-console.log('hello snake');
+ /*----- constants -----*/
+const boardSize = 20; // 20 squares by 20 squares
+
+// what is this weird behavior? i didn't cache board element but this is working?
+const numOfCells = boardSize**2;
+for (let i = 1; i <= numOfCells; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    cell.setAttribute('id',`${i}`)
+    board.appendChild(cell);
+ }
+ /*----- state variables -----*/
+let snake, snakeLength, foodPosition, IsGameOver;
+
+ /*----- cached elements  -----*/
+// let board = document.getElementById('board');
+// let cells1 = document.querySelectorAll('.cell');
+let cells = [...document.querySelectorAll('.cell')];
+
+ /*----- event listeners -----*/
+
+// listen for key up down left right arrow key
+
+ /*----- functions -----*/
+
+ function randomizePosition() {
+    return Math.floor(Math.random() * numOfCells);
+}
+
+function initialize() {
+    snake = [1]; // gets longer by pushing 1 into it
+    // snakeLength = 1; - probably don't need?
+    foodPosition = randomizePosition();
+    IsGameOver = false;
+    // initializeFoodPosition();
+    // initializeSnakePosition();
+    // render functions here to update all the states in the DOM
+}
+
+initialize();
+
+function initializeFoodPosition() {
+    let indexOfFood = cells.indexOf(cells[foodPosition]);
+    cells[indexOfFood].style.backgroundColor = 'red';
+}
+
+function initializeSnakePosition() {
+    // let snakePosition = randomizePosition();
+    // let indexOfSnake = cells.indexOf(cells[snakePosition]);
+    // cells[indexOfSnake].style.backgroundColor = 'green';
+}
+
+    let snakePosition = randomizePosition();
+    console.log(snakePosition);
+    // let indexOfSnake = cells.indexOf(cells[snakePosition]);
+
+    // let indexOfSnake = cells.forEach((cell,idx) => {  // use find ???
+    //     // if (cell.id == idx) console.log(cell.id)
+    //     if (idx === snakePosition +1) {
+    //         // console.log(snakePosition + 1)
+    //         // let res = snakePosition + 1;
+    //         return 'hi';
+    //     }
+    //     // console.log(idx === snakePosition +1)
+    // })
+
+    // let indexOfSnake = cells.find(cell => cell.id === snakePosition+1);
+    // let indexOfSnake = cells.find(cell => {
+    //     if (Number(cell.id) === snakePosition+1) return Number(cell.id);
+    // });
+    // let indexOfSnake = cells.findIndex(cell => {
+    //     // get the correct index
+    //     return Number(cell.id) === snakePosition+1
+    // });
+
+    let indexOfSnake = cells.indexOf(cells[snakePosition]);  
+    console.log(indexOfSnake);
+    cells[indexOfSnake].style.backgroundColor = 'green';
+
+    // function getCellPosition() {
+    //     cells.forEach((cell,idx) => {
+    //         // if (cell.id === cells.indexOf(cells[snakePosition]) + 1) return cell.id;
+    //         if ( cells.indexOf(cells[snakePosition]) ===cell.id) return cell.id;
+    //         // console.log(idx)
+    //     })
+    // }
+    // let cellPosition = getCellPosition();
+    // console.log(cellPosition);
+
+// setInterval(move, 1000)
+
+function move() {
+    cells[indexOfSnake].style.backgroundColor = '';
+    // moving right
+    cells[indexOfSnake+1].style.backgroundColor = 'green';
+    indexOfSnake += 1;
+    if (indexOfSnake % 20 === 0) {
+        // cells[indexOfSnake].style.backgroundColor = '';
+        // indexOfSnake -= 20;
+    }
+    console.log(indexOfSnake)
+    // // moving left
+    // cells[indexOfSnake-1].style.backgroundColor = 'green';
+    // // moving up
+    // cells[indexOfSnake-20].style.backgroundColor = 'green';
+    // // movig down
+    // cells[indexOfSnake+20].style.backgroundColor = 'green';
+}

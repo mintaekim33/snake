294fef2225e5f083f9ed328457359e620a6740ed
mintaekim <kmintae33@gmail.com>
2023-09-26 22:02:53 +0800

    regnerate food

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
  /*----- constants -----*/
 const boardSize = 20; // 20 squares by 20 squares
 
-// what is this weird behavior? i didn't cache board element but this is working?
 const numOfCells = boardSize**2;
 for (let i = 1; i <= numOfCells; i++) {
     const cell = document.createElement('div');
@@ -9,10 +8,9 @@ for (let i = 1; i <= numOfCells; i++) {
     cell.setAttribute('id',`${i}`)
     board.appendChild(cell);
  }
-// acutally don't even need to add these cells lol
 
  /*----- state variables -----*/
-let snake, snakeLength, foodPosition, IsGameOver;
+let snakePosition, snakeArray, foodPosition, IsGameOver;
 
  /*----- cached elements  -----*/
 // let board = document.getElementById('board');
@@ -21,7 +19,7 @@ let cells = [...document.querySelectorAll('.cell')];
 
  /*----- event listeners -----*/
 
-// listen for key up down left right arrow key
+ document.addEventListener('keydown', handleKeys);
 
  /*----- functions -----*/
 
@@ -30,132 +28,131 @@ let cells = [...document.querySelectorAll('.cell')];
 }
 
 function initialize() {
-    snake = [1]; // gets longer by pushing 1 into it
+    snakeArray = [1]; // gets longer by pushing 1 into it
     // snakeLength = 1; - probably don't need?
-    foodPosition = randomizePosition();
     IsGameOver = false;
-    // initializeFoodPosition();
-    // initializeSnakePosition();
+    initializeFoodPosition();
+    initializeSnakePosition();
     // render functions here to update all the states in the DOM
 }
 
 initialize();
 
 function initializeFoodPosition() {
-    let indexOfFood = cells.indexOf(cells[foodPosition]);
-    cells[indexOfFood].style.backgroundColor = 'red';
+    foodPosition = randomizePosition();
+    cells[foodPosition].style.backgroundColor = 'red';
+    // cells[foodPosition].classList.add('food');
 }
 
 function initializeSnakePosition() {
-    // let snakePosition = randomizePosition();
-    // let indexOfSnake = cells.indexOf(cells[snakePosition]);
-    // cells[indexOfSnake].style.backgroundColor = 'green';
+    snakePosition = randomizePosition();
+    cells[snakePosition].style.backgroundColor = 'green';
+    // cells[snakePosition].classList.add('snake');
 }
-
-    let snakePosition = randomizePosition();
-    let indexOfSnake = cells.indexOf(cells[snakePosition]);
-    cells[indexOfSnake].style.backgroundColor = 'green';
-
-
 
 // let intervalId = setInterval(moveLeft, 500) - put this in the move updownleftright functions
 
-// function move() {
-//     document.onkeydown = function(e) {
-//         switch (e.key) {
-//             case "ArrowLeft":
-//                 moveLeft();
-//                 break;
-//             case "ArrowRight":
-//                 moveRight();
-//                 break;
-//             case "ArrowUp":
-//                 moveUp();
-//                 break;
-//             case "ArrowDown":
-//                 moveDown();
-//                 break;
-//         }
-//     }
-// }
-// function move(e) {
-//             switch (e.key) {
-//             case "ArrowLeft":
-//                 moveLeft();
-//                 break;
-//             case "ArrowRight":
-//                 moveRight();
-//                 break;
-//             case "ArrowUp":
-//                 moveUp();
-//                 break;
-//             case "ArrowDown":
-//                 moveDown();
-//                 break;
-//         }
-// }
+function handleKeys(e) {
+    // save previous snake postion for the body to follow
+    let prevPosition = snakePosition;
+    console.log(prevPosition)
 
-// document.addEventListener('keydown', move(e));
-document.addEventListener('keydown', function(e) {
+    // remove snake class
+    cells[snakePosition].style.backgroundColor = '';
+    // cells[snakePosition].classList.remove('snake');
     const key = e.key;
     switch (key) { 
         case "ArrowLeft":
-            moveLeft();
+            snakePosition -= 1;
             break;
         case "ArrowRight":
-            moveRight();
+            // moveRight();
+            snakePosition += 1;
             break;
         case "ArrowUp":
-            moveUp();
+            // moveUp();
+            snakePosition -= 20;
             break;
         case "ArrowDown":
-            moveDown();
+            // moveDown();
+            snakePosition += 20;
             break;
     }
-} );
+    // add snake class
+    cells[snakePosition].style.backgroundColor = 'green';
+    // cells[snakePosition].classList.add('snake');
 
-function moveRight() {
+    console.log("snake pos: ", snakePosition)
+    console.log("prevposition: ", prevPosition)
+
+    if (snakePosition === foodPosition) {
+        snakeArray.push(1);
+        console.log(snakeArray.length);
+        initializeFoodPosition();
+
+    }
+
+    for (let i=0; i<snakeArray.length-1; i++) {
+        console.log(i) // why is it not incrementing?
+        cells[prevPosition].style.backgroundColor = '';
+        cells[snakePosition].style.backgroundColor = 'green';
+
+        
+    }
+}
+
+// create body
+function eat() {}
+
+// auto move
+// collision (walls & itself)
+
+
+
+
+
+// function moveRight() {
     
-    let move = () => {
-        console.log(indexOfSnake)
-        cells[indexOfSnake].style.backgroundColor = '';
-        cells[indexOfSnake+1].style.backgroundColor = 'green';
-        indexOfSnake += 1;
-        if (indexOfSnake % 20 === 0) {
-            cells[indexOfSnake].style.backgroundColor = '';
-            clearInterval(intervalId);
-            // cells[indexOfSnake-1].style.backgroundColor = 'green';
-    }
-    }
-    let intervalId = setInterval(move, 1000)
-}
+//     // let move = () => {
+//         cells[indexOfSnake].style.backgroundColor = '';
+//         cells[indexOfSnake+1].style.backgroundColor = 'green';
+//         indexOfSnake += 1;
+//         // if (indexOfSnake % 20 === 0) {
+//         //     cells[indexOfSnake].style.backgroundColor = '';
+//         //     clearInterval(intervalId);
+//         //     // cells[indexOfSnake-1].style.backgroundColor = 'green';
+//         // }
+//     // }
+//     // let intervalId = setInterval(move, 1000)
+// }
 
-function moveLeft() {
-    cells[indexOfSnake].style.backgroundColor = '';
-    cells[indexOfSnake-1].style.backgroundColor = 'green';
-    indexOfSnake -= 1;
-    if (indexOfSnake % 20 === 0) {
-        // cells[indexOfSnake].style.backgroundColor = '';
-        clearInterval(intervalId);
-    }
-}
+// function moveLeft() {
+    
+//     cells[indexOfSnake].style.backgroundColor = '';
+//     cells[indexOfSnake-1].style.backgroundColor = 'green';
+//     indexOfSnake -= 1;
+//     // if (indexOfSnake % 20 === 0) {
+//     //     // cells[indexOfSnake].style.backgroundColor = '';
+//     //     clearInterval(intervalId);
+//     // }
+// }
 
-function moveUp() {
-    cells[indexOfSnake].style.backgroundColor = '';
-    cells[indexOfSnake-20].style.backgroundColor = 'green';
-    indexOfSnake -= 20;
-    if (indexOfSnake < 20) {
-        // cells[indexOfSnake].style.backgroundColor = '';
-        clearInterval(intervalId);
-    }
-}
+// function moveUp() {
+//     cells[indexOfSnake].style.backgroundColor = '';
+//     cells[indexOfSnake-20].style.backgroundColor = 'green';
+//     indexOfSnake -= 20;
+//     // if (indexOfSnake < 20) {
+//     //     // cells[indexOfSnake].style.backgroundColor = '';
+//     //     clearInterval(intervalId);
+//     // }
+// }
 
-function moveDown() {
-    cells[indexOfSnake].style.backgroundColor = '';
-    cells[indexOfSnake+20].style.backgroundColor = 'green';
-    indexOfSnake += 20;
-    if (indexOfSnake > 380) {
-        // cells[indexOfSnake].style.backgroundColor = '';
-        clearInterval(intervalId);
-    }
-}
+// function moveDown() {
+//     cells[indexOfSnake].style.backgroundColor = '';
+//     cells[indexOfSnake+20].style.backgroundColor = 'green';
+//     indexOfSnake += 20;
+//     // if (indexOfSnake > 380) {
+//     //     // cells[indexOfSnake].style.backgroundColor = '';
+//     //     clearInterval(intervalId);
+//     // }
+// }

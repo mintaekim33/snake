58bbeadc31259ee00c88b72f4d95b85d2dcfa0dd
mintaekim <kmintae33@gmail.com>
2023-10-01 23:04:35 +0800

    refactor code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ for (let i = 1; i <= numOfCells; i++) {
 }
 
 /*----- state variables -----*/
-let snakePosition, snakeArray, foodPosition, IsGameOver, direction;
+let snakePosition, snakeArray, foodPosition, IsGameOver, direction, speed;
 let prevPositionArray = [];
 
 /*----- cached elements  -----*/
@@ -21,6 +21,7 @@ let gameOverScreen = document.querySelector('.gameOverScreen');
 let gameOverMsg = document.getElementById('gameOver');
 let resetBtn = document.querySelector('.reset');
 let level = document.querySelector('.level');
+let levelUp = document.getElementById('levelUp');
 
 /*----- event listeners -----*/
 
@@ -36,22 +37,20 @@ function randomizePosition() {
 function initialize() {
   snakeArray = [1];
   IsGameOver = false;
+  speed = 200;
   initializeFoodPosition();
   initializeSnakePosition();
-  // render functions here to update all the states in the DOM
 }
 
 initialize();
 
 function initializeFoodPosition() {
   foodPosition = randomizePosition();
-  // cells[foodPosition].style.backgroundColor = 'red';
   cells[foodPosition].classList.add("food");
 }
 
 function initializeSnakePosition() {
   snakePosition = randomizePosition();
-  // cells[snakePosition].style.backgroundColor = 'green';
   cells[snakePosition].classList.add("snake");
 }
 
@@ -62,99 +61,96 @@ function handleKeys(e) {
             if (direction === "left" || direction === "right") return;
             direction = "left";
             break;
-            case "ArrowRight":
-                if (direction === "left" || direction === "right") return;
-                direction = "right";
-                break;
-                case "ArrowUp":
-      if (direction === "up" || direction === "down") return;
-      direction = "up";
-      break;
-    case "ArrowDown":
-        if (direction === "up" || direction === "down") return;
-        direction = "down";
-        break;
+        case "ArrowRight":
+            if (direction === "left" || direction === "right") return;
+            direction = "right";
+            break;
+        case "ArrowUp":
+            if (direction === "up" || direction === "down") return;
+            direction = "up";
+            break;
+        case "ArrowDown":
+            if (direction === "up" || direction === "down") return;
+            direction = "down";
+            break;
     }
+}
+
+function moveSnake() {
+    prevPositionArray.push(snakePosition);
+    prevPositionArray.forEach((pos) => {
+      cells[pos].classList.remove("snake");
+    });
+    prevPositionArray = prevPositionArray.slice(-snakeArray.length);
+    // const eatenFlag = eat(snakePosition);
+    // if (!eatenFlag) {
+    //   let removed = prevPositionArray.shift();
+    //   cells[removed].classList.remove("snake");
+    // }
+    prevPositionArray.forEach((pos) => {
+      cells[pos].classList.add("snake");
+    });
 }
 
 let intervalId;
 
 intervalId = setInterval(function() {
 
-  eat(snakePosition);
-
-            // const eatenFlag = eat(snakePosition);
-            // if (!eatenFlag) {
-            //   let removed = prevPositionArray.shift();
-            //   cells[removed].classList.remove("snake");
-            // }
+//   eat(snakePosition);
+eat();
 
   if (direction === "left") {
     cells[snakePosition].classList.remove("snake");
     let storedPrevPos = snakePosition;
     snakePosition -= 1;
     checkCollision(storedPrevPos);
-    prevPositionArray.push(snakePosition);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.remove("snake");
-    });
-    prevPositionArray = prevPositionArray.slice(-snakeArray.length);
-            // const eatenFlag = eat(snakePosition);
-            // if (!eatenFlag) {
-            //   let removed = prevPositionArray.shift();
-            //   cells[removed].classList.remove("snake");
-            // }
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.add("snake");
-    });
+    moveSnake();
   } else if (direction === "right") {
     cells[snakePosition].classList.remove("snake");
     let storedPrevPos = snakePosition;
     snakePosition += 1;
     checkCollision(storedPrevPos);
-    prevPositionArray.push(snakePosition);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.remove("snake");
-    });
-    prevPositionArray = prevPositionArray.slice(-snakeArray.length);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.add("snake");
-    });
+    moveSnake();
   } else if (direction === "up") {
     cells[snakePosition].classList.remove("snake");
     snakePosition -= 20;
     checkCollision();
-    prevPositionArray.push(snakePosition);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.remove("snake");
-    });
-    prevPositionArray = prevPositionArray.slice(-snakeArray.length);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.add("snake");
-    });
+    moveSnake();
   } else if (direction === "down") {
     cells[snakePosition].classList.remove("snake");
     snakePosition += 20;
     checkCollision();
-    prevPositionArray.push(snakePosition);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.remove("snake");
-    });
-    prevPositionArray = prevPositionArray.slice(-snakeArray.length);
-    prevPositionArray.forEach((pos) => {
-      cells[pos].classList.add("snake");
-    });
+    moveSnake();
   }
-}, 100);
+}, speed);
 
-function eat(currentSnakePosition) {
-    if (currentSnakePosition === foodPosition) {
+// function eat(currentSnakePosition) {
+//     if (currentSnakePosition === foodPosition) {
+//         snakeArray.push(1);
+//         cells[foodPosition].classList.remove("food");
+//         initializeFoodPosition();
+//         levelUp.textContent = prevPositionArray.length;
+//         if (prevPositionArray.length === 5) {
+//             console.log("first")
+//             speed = 100;
+//         }
+//         return true;
+//     }
+//     return false;
+// }
+
+function eat() {
+    if (snakePosition === foodPosition) {
         snakeArray.push(1);
         cells[foodPosition].classList.remove("food");
         initializeFoodPosition();
+        levelUp.textContent = prevPositionArray.length;
+        if (prevPositionArray.length === 5) {
+            console.log("first")
+            speed = 100;
+        }
         return true;
     }
-    return false;
 }
 
 function checkCollision(storedPrevPos) {
@@ -165,7 +161,6 @@ function checkCollision(storedPrevPos) {
     ((storedPrevPos + 1) % 20 === 0 && direction === "right") || // checking right edge
     cells[snakePosition].classList.contains("snake") // checking body collision
   ) {
-    // console.log("Game over");
     IsGameOver = true;
     clearInterval(intervalId);
     level.style.display = 'none';
